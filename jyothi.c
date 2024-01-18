f1ad0bcbc53027a7a8dc9d68251584647e5287e4
SunnaparalaSujatha <155890515+SunnaparalaSujatha@users.noreply.github.com>
2024-01-18 15:56:39 +0530

    Add files via upload

---
+++ jyothi.c
@@ -0,0 +1,15 @@
+//program to find simple interest
+//simple interest=PTR/100
+#include<stdio.h>
+int main(){
+    int principal,time,rate;
+    print f("enter prinipal");
+    scan f("%d",&principal);
+    print f("enter time");
+    scan f("%d",&time);
+    print f("enter rate");
+    scan f("%d",&rate);
+    int SI;
+    SI=(principal*time*rate)/100;
+    print f("simple interest:%d",SI);
+}
